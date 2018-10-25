1df046ea07d317674392955c901c7a4bec4a276d
DamenaShojoBronya <43756924+DamenaShojoBronya@users.noreply.github.com>
2018-10-25 23:33:28 +0800

    Add files via upload

---
+++ PTA/PAT -Basic Level- Practice/1011.cpp
@@ -0,0 +1,12 @@
+﻿#include <cstdio>
+int main()
+{
+	long int t = 1, a = 0, b = 0, c = 0;
+	scanf("%d", &t); 
+	for (int i = t + 1; t > 0; t--) {
+		scanf("%ld%ld%ld", &a, &b, &c);
+		if (a + b > c) printf("Case #%d: true\n", i - t);
+		else printf("Case #%d: false\n", i - t); 
+	} 
+	return 0;
+}
