d2f341a8dad11774fdc707266b4bfde89d78b203
DamenaShojoBronya <43756924+DamenaShojoBronya@users.noreply.github.com>
2018-10-25 23:32:57 +0800

    Add files via upload

---
+++ PTA/PAT -Basic Level- Practice/1009.cpp
@@ -0,0 +1,36 @@
+﻿#include <string>
+#include <iostream>
+#include <sstream>
+#pragma warning(disable:4996)
+using namespace std;
+
+int main()
+{
+	string words[81], data; char temp[81];
+	char data_temp[81] = { 0 };
+	int i, j = 0, num = 0, count = 0;
+
+	getline(cin, data);
+
+	for (i = 0; ; i++)
+	{
+		for (j = count; data[j] != NULL; j++)
+		{
+			if (count > 80) return 0;
+			if (data[j] == ' ') { words[i] = data_temp; count++; break; }
+			data_temp[num] = data[j]; num++;
+			count++;
+		}
+		if (data[j] == NULL) { words[i] = data_temp; break; }
+		for (int i1 = 0; i1 < 81; i1++) data_temp[i1] = 0;
+		num = 0;
+	}
+
+	for (int i1 = i; i1 >= 0; i1--)
+	{
+		if (i1 == 0) { cout << words[i1]; break; }
+		cout << words[i1] << " ";
+	}
+
+	return 0;
+}
---
+++ PTA/PAT -Basic Level- Practice/1010.cpp
@@ -0,0 +1,27 @@
+﻿#include<stdio.h>
+int main()
+{
+	int i, coefficient[100], index[100];
+	for (i = 0; ; i++)
+	{
+		scanf_s("%d %d", &coefficient[i], &index[i]);
+		char a;
+		scanf_s("%c", &a);
+		if (a == '\n') break;
+	}
+	for (int i1 = 0; i1 <= i; i1++)
+	{
+		if (coefficient[i1] > 1000 || coefficient[i1] < -1000) return 0;
+		if (index[i1] > 1000 || index[i1] < -1000) return 0;
+
+		if (index[i1] == 0 && coefficient[i1] == 0) printf("0 0");
+		if (index[i1 + 1] != 0)
+		{
+			if (i1 != i && index[i1] != 0) printf("%d %d ", coefficient[i1] * index[i1], index[i1] - 1);
+			if (i1 == i && index[i1] != 0) printf("%d %d", coefficient[i1] * index[i1], index[i1] - 1);
+		}
+		else
+			if (coefficient[i1] != 0)  printf("%d %d", coefficient[i1] * index[i1], index[i1] - 1);
+	}
+	return 0;
+}
