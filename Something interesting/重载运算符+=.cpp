362de0821d2e167f1dfc82bf5cd7393e3117b899
DamenaShojoBronya <43756924+DamenaShojoBronya@users.noreply.github.com>
2018-10-26 23:55:45 +0800

    Add files via upload

---
+++ Something interesting/重载运算符+=.cpp
@@ -0,0 +1,36 @@
+#include <iostream>  
+#include <string>   
+using namespace std;   
+
+class cstring;  
+ostream& operator<<(ostream &output, const cstring &s);
+
+class cstring  
+{
+private:
+    char s[100];  
+public:  
+    cstring(const char* str){strcpy(s,str);}    
+    friend cstring& operator +=(cstring& str1, const cstring& str2);
+	friend ostream& operator <<(ostream& output, const cstring&); 
+};  
+
+cstring& operator +=(cstring& str1, const cstring& str2)    
+{  
+    strcat(str1.s,str2.s);    
+    return str1;  
+}  
+
+ostream& operator<<(ostream &output, const cstring &ob){  
+    output<<ob.s;  
+    return output; 
+}   
+
+int main()  
+{  
+    cstring str1("laozhu,"),str2("dongleba");  
+    str1 += str2;   
+    cout<<str1<<endl;
+	cout<<str2<<endl;
+    return 0;  
+}  
