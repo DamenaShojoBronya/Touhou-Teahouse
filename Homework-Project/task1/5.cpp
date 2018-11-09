2382aef37f77be69c6cd83ce43e14e69fd68cafb
DamenaShojoBronya <43756924+DamenaShojoBronya@users.noreply.github.com>
2018-11-09 21:51:38 +0800

    Update 5.cpp

--- Homework-Project/task1/5.cpp
+++ Homework-Project/task1/5.cpp
@@ -1,5 +1,6 @@
-/*Write a program that reads the subtotoal and the gratuity rate,then computes the gratuity and total.
-For example,if the user enters 10 for subtotal and 15% for gratuity rate,the program displays $1.5 as gratuity and $11.5 as total.*/
+/*	Write a program that reads the subtotoal and the gratuity rate,then computes the gratuity and total.
+	For example,if the user enters 10 for subtotal and 15% for gratuity rate,the program displays $1.5 as gratuity and $11.5 as total.	*/
+
 #include<iostream>
 using namespace std;
 #define RESULT subtotal*gratuity_rate/100
